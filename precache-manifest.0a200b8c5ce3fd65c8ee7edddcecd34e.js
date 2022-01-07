1a6b176f67551314e17632400c32abbbea0e918c
Steven Yao <stevenyao@Stevens-MacBook-Air.local>
2022-01-07 00:13:08 -0600

    Updates

---
+++ precache-manifest.0a200b8c5ce3fd65c8ee7edddcecd34e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45cf31c6187c53730c645e4882f1eda",
+    "url": "/skyao2002/gamepigeon-frontend/index.html"
+  },
+  {
+    "revision": "69ea018db231e97ab2af",
+    "url": "/skyao2002/gamepigeon-frontend/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "b7b77e1ca298759f05b6",
+    "url": "/skyao2002/gamepigeon-frontend/static/css/main.e6aab7b7.chunk.css"
+  },
+  {
+    "revision": "69ea018db231e97ab2af",
+    "url": "/skyao2002/gamepigeon-frontend/static/js/2.8e4c4897.chunk.js"
+  },
+  {
+    "revision": "e3e71bde41bc4cb7f24567ddcedf6179",
+    "url": "/skyao2002/gamepigeon-frontend/static/js/2.8e4c4897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b77e1ca298759f05b6",
+    "url": "/skyao2002/gamepigeon-frontend/static/js/main.6ce240bb.chunk.js"
+  },
+  {
+    "revision": "0e303bf47c01a20979c9",
+    "url": "/skyao2002/gamepigeon-frontend/static/js/runtime-main.83d7aed4.js"
+  }
+]);
